a9e5f1cb656b75739d08b09849729de7679d00a9
hanshenrik <hh.gronsleth@gmail.com>
2015-12-18 16:06:50 +0100

    Some golfing

--- x-mas-tree-shortest-contest.py
+++ x-mas-tree-shortest-contest.py
@@ -1,3 +1,3 @@
 import sys
-n=int(sys.argv[1])if len(sys.argv)>1 else 7
-[print(x.center(n*2))for x in[((2*i+1)*'*')if i<n else'|'for i in range(n+2)]]
+n=len(sys.argv)>1 and int(sys.argv[1])or 7
+[print(x.center(n*2))for x in[('|',((2*i+1)*'*'))[i<n]for i in range(n+2)]]
